4c7f38dcd76a8fae8b1ae1caca1a8adbc0df422c
AmrSa2002 <128056206+AmrSa2002@users.noreply.github.com>
2026-03-16 11:28:33 +0100

    prva vjezba

---
+++ ooad_lv1.js
@@ -0,0 +1,12 @@
+function saberi(a, b) {
+    return a + b;
+}
+
+let broj1 = 5;
+let broj2 = 7;
+
+let rezultat = saberi(broj1, broj2);
+
+console.log("Prvi broj:", broj1);
+console.log("Drugi broj:", broj2);
+console.log("Rezultat sabiranja:", rezultat);
